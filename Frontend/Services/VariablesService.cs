ba495c35579ad0de4dbac23a80d79a1eb396a93d
Éwerton Ferreira <eeeeewerton@gmail.com>
2024-02-13 22:38:59 -0300

    Initial screens drafts

---
+++ Frontend/Services/VariablesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Hephaestus.Frontend.Services {
+
+	public class VariablesService {
+
+		public static string SaudationCode() {
+
+			var time = DateTime.Now.TimeOfDay;
+			var afternoon = new TimeSpan(12, 0, 0);
+			var evening = new TimeSpan(18, 0, 0);
+			var night = new TimeSpan(20, 0, 0);
+
+			return
+				time < afternoon ? "0050" :
+				time < evening ? "0051" :
+				time < night ? "0052"
+				: "0053";
+
+		}
+
+	}
+
+}
