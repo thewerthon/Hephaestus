a667d3e23dd42ed00679f6eeb0fc283a1d958b5f
thewerthon <eeeeewerton@gmail.com>
2024-02-19 17:03:06 -0300

    Basic log model

--- Architect/Common/Models/Entities/BaseEntityTraceable.cs
+++ Architect/Common/Models/Entities/BaseEntityTraceable.cs
@@ -4,21 +4,21 @@ public abstract class BaseEntityTraceable : BaseEntity, IEntityTraceable {
 
 	[ForeignKey("Active")]
 	public bool Active { get; set; }
-	public YesNo? ActiveData { get; set; }
+	public virtual YesNo? ActiveData { get; set; }
 
 	[ForeignKey("CreatedBy")]
 	public int? CreatedBy { get; set; }
-	public User? CreatedByData { get; set; }
+	public virtual User? CreatedByData { get; set; }
 	public DateTime? CreatedOn { get; set; }
 
 	[ForeignKey("UpdatedBy")]
 	public int? UpdatedBy { get; set; }
-	public User? UpdatedByData { get; set; }
+	public virtual User? UpdatedByData { get; set; }
 	public DateTime? UpdatedOn { get; set; }
 
 	[ForeignKey("DeletedBy")]
 	public int? DeletedBy { get; set; }
-	public User? DeletedByData { get; set; }
+	public virtual User? DeletedByData { get; set; }
 	public DateTime? DeletedOn { get; set; }
 
 }
---
+++ Architect/System/Models/Entities/Log.cs
@@ -0,0 +1,25 @@
+﻿namespace Hephaestus.Architect.Application.Models;
+
+public class Log : BaseEntity {
+
+	[Required]
+	[MaxLength(64)]
+	public string Action { get; set; } = string.Empty;
+
+	[MaxLength(64)]
+	public string? ComputerName { get; set; } = string.Empty;
+
+	[MaxLength(64)]
+	public string? IPAddress { get; set; } = string.Empty;
+
+	[Required]
+	[ForeignKey("Version")]
+	public Version Version { get; set; } = 0;
+	public virtual Version? VersionData { get; set; }
+
+	[ForeignKey("PerformedBy")]
+	public int? PerformedBy { get; set; }
+	public virtual User? PerformedByData { get; set; }
+	public DateTime? PerformedOn { get; set; }
+
+}
